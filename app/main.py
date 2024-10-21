ca9f69fc19b48d72f1a3579f167417b903ded58f
AdbulrhmanEldeeb <abdodebo3@gmail.com>
2024-10-21 09:48:38 +0300

    code formatted black

--- app/main.py
+++ app/main.py
@@ -1,19 +1,21 @@
-from fastapi import FastAPI , Query 
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
 from app.model.model import predict_pipeline
 from app.model.model import __version__ as model_version
-from typing import Annotated 
+from typing import Annotated
 
 app = FastAPI()
 
 
 class ValuesInput(BaseModel):
-    value1: Annotated[float , Query(gt=0)]
-    value2: Annotated[float , Query(gt=0)]
-    value3: Annotated[float , Query(gt=0)]
-    value4: Annotated[float , Query(gt=0)]
-    class Config : 
-        extra="forbid"
+    value1: Annotated[float, Query(gt=0)]
+    value2: Annotated[float, Query(gt=0)]
+    value3: Annotated[float, Query(gt=0)]
+    value4: Annotated[float, Query(gt=0)]
+
+    class Config:
+        extra = "forbid"
+
 
 class PredictionOut(BaseModel):
     class_name: str
--- app/model/model.py
+++ app/model/model.py
@@ -1,6 +1,7 @@
 from pathlib import Path
-import joblib 
-import numpy as np 
+import joblib
+import numpy as np
+
 __version__ = "0.1.0"
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent
@@ -10,15 +11,16 @@ with open(f"{BASE_DIR}/trained_pipeline-{__version__}.joblib", "rb") as f:
     model = joblib.load(f)
 
 
-classes =  ['setosa', 'versicolor', 'virginica']
+classes = ["setosa", "versicolor", "virginica"]
 
 
 def predict_pipeline(payload):
-    
+
     input_values = np.array(
-    [[payload.value1, payload.value2, payload.value3, payload.value4]], dtype=np.float16
-)
-    
+        [[payload.value1, payload.value2, payload.value3, payload.value4]],
+        dtype=np.float16,
+    )
+
     result = classes[int(model.predict(input_values)[0])]
 
-    return  result
+    return result
