77004ed656909b038a3c3e088b619f650d125a18
AdbulrhmanEldeeb <abdodebo3@gmail.com>
2024-10-21 17:05:10 +0300

    Fix port binding issue for Heroku

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel
 from app.model.model import predict_pipeline
 from app.model.model import __version__ as model_version
 from typing import Annotated
+import uvicorn
+import os 
 
 app = FastAPI()
 
@@ -26,3 +28,7 @@ def home():
 def predict(payload: ValuesInput):
     result = predict_pipeline(payload)
     return {"class_name": result}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
